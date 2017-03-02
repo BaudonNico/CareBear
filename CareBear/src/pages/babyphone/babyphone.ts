fd5e1f13062623af36fbff3a4fd26fa319900667
patrick-randria <patrick2015>
2017-03-02 17:00:56 +0100

    Listen to audio stream

--- CareBear/src/pages/babyphone/babyphone.ts
+++ CareBear/src/pages/babyphone/babyphone.ts
@@ -11,11 +11,40 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'babyphone.html'
 })
 export class BabyphonePage {
+  url:string;
+  stream:any;
+  promise:any;
 
   constructor(public navCtrl: NavController) {
+    //this.url = "http://akalmultimedia.net:8000/GDNSLDH";
+    this.url = "http://www.opticodec.com/test/tropic.m4a";
+    this.stream = new Audio(this.url);
+
+    //this.playStream();
+  }
+
+  playStream() {
+    this.stream.play();
+    this.promise = new Promise((resolve,reject) => {
+      this.stream.addEventListener('playing', () => {
+        resolve(true);
+      });
+
+      this.stream.addEventListener('error', () => {
+        reject(false);
+      });
+    });
+
+    return this.promise;
+  };
+
+  pauseStream() {
+    this.stream.pause();
+  };
 
 
-
+  uploadStream() {
+    this.stream.pause();
   }
 
 }
